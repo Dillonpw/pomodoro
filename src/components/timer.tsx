e96a293daadceba9e3d8ec38a451e9c0ced8191e
dillonpw <dillonpwalsh10@gmail.com>
2024-03-22 11:29:28 -0400

    updated functionality

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { useTimer } from 'react-timer-hook';
+import React from "react";
+import { useTimer } from "react-timer-hook";
 
 const PomodoroTimer = () => {
-  const expiryTimestamp = new Date();
-  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 1 * 60); // 25 minutes from now
 
-  const {
-    seconds,
-    minutes,
-    start,
-    pause,
-    restart,
-  } = useTimer({
+  const expiryTimestamp = new Date();
+  expiryTimestamp.setSeconds(
+    expiryTimestamp.getSeconds() + { pomodoroLength } * 60,
+  ); // 25 minutes from now
+
+  const { seconds, minutes, start, pause, restart } = useTimer({
     expiryTimestamp,
-    onExpire: () => alert('Time is up!'),
+    onExpire: () => alert("Time is up!"),
   });
 
   // Restart timer function to reset it to 25 minutes
   const restartTimer = () => {
     const newExpiryTimestamp = new Date();
-    newExpiryTimestamp.setSeconds(newExpiryTimestamp.getSeconds() + 1 * 60);
+    newExpiryTimestamp.setSeconds(
+      newExpiryTimestamp.getSeconds() + { pomodoroLength } * 60,
+    );
     restart(newExpiryTimestamp);
   };
 
@@ -32,7 +31,8 @@ const PomodoroTimer = () => {
     <div>
       <h1>Pomodoro Timer</h1>
       <div>
-        <span>{minutes}</span>:<span>{seconds < 10 ? `0${seconds}` : seconds}</span>
+        <span>{minutes}</span>:
+        <span>{seconds < 10 ? `0${seconds}` : seconds}</span>
       </div>
       <button onClick={pause}>Pause</button>
       <button onClick={start}>Start</button>
